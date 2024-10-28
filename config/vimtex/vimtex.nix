ca37234c6bf06f24253ece3898be118fed37530e
bloodwolfepc <bloodwolfepc@gmail.com>
2024-10-28 18:36:12 -0500

    commit

--- config/vimtex/vimtex.nix
+++ config/vimtex/vimtex.nix
@@ -5,6 +5,17 @@
   config = lib.mkIf config.configured.vimtex.enable {
     plugins.vimtex = {
       enable = true;
+      settings = {
+        view_method = "zathura";
+        quickfix_enabled = true;
+        quickfix_open_on_warinig = false;
+        quickfix_ignore_filters = [
+          "Underfull"
+          "Overfull"
+          "specifier changed to"
+          "Token not allowed in a PDF string"
+        ];
+      };
     };
   };
 }
